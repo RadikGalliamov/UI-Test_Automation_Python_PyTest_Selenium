ad9cebdcce83f1a222b42bdcdddac0903e4c0213
RodeonGG <galliamovrr@gmail.com>
2022-03-22 21:56:59 +0500

    Созданы файлы conftest, init, test_main_page - файл conftest создана функция pytest_addoption - выбор параметров теста в командной строке, функция browser - параметры браузера для каждого теста - файл test_main_page c методами для главной страницы продукта

---
+++ conftest.py
@@ -0,0 +1,30 @@
+# Общая подготовка перед запуском тестов (методы 'до' и 'после' запуска основных тестов,
+# параметры запуска в командной строке)
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# добавляем параметр запуска тестов в командной строке(выбор браузера, по умолчанию хром, языка)
+def pytest_addoption(parser):
+    # добавляем параметр запуска тестов в командной строке(чем запускать, хромом или фаерфоксом) По умолчанию хром
+    # parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or firefox")
+    # Можно задать значение параметра по умолчанию,
+    # чтобы в командной строке не обязательно было указывать параметр --browser_name, например, так:
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help='Choose language browser: ru,en,fr...')
+
+
+# Запуск браузера(для каждой функции)
+@pytest.fixture(scope="function")  # запускается для каждой функции
+def browser(request):
+    language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)  # До тестов
+    yield browser
+    browser.quit()  # После тестов
+
+#   Добавить запуски тестов с FireFox
---
+++ test_main_page.py
@@ -0,0 +1,43 @@
+# Тест-кейсы основной страницы
+
+
+from .pages.main_page import MainPage  # импортируем проверки для главной страницы
+from .pages.login_page import LoginPage  # импортируем проверки для страницы логина/регистрации
+
+link = "http://selenium1py.pythonanywhere.com/"
+link_login_page = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+
+# Метод проверки наличия страницы логина (гость) (MainPage)
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+
+# Метод проверки ссылки на логин (гость) (MainPage)
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()  # выполняем метод - проверки наличия страницы логина
+
+
+# Метод корректный ли url адрес ("login" есть в текущем url браузере), (гость) (LoginPage)
+def test_guest_can_should_be_login_url(browser):
+    page = LoginPage(browser, link_login_page)
+    page.open()
+    page.should_be_login_url()  # выполняем метод - проверки ссылки на логин
+
+
+# Метод проверки формы регистрации (гость) (LoginPage)
+def test_guest_should_be_register_form(browser):
+    page = LoginPage(browser, link_login_page)
+    page.open()
+    page.should_be_register_form()  # выполняем метод - проверки формы регистрации
+
+
+# Метод проверки формы логина (user) (LoginPage)
+def test_user_should_be_login_form(browser):
+    page = LoginPage(browser, link_login_page)
+    page.open()
+    page.should_be_login_form()  # выполняем метод - проверки формы логина
